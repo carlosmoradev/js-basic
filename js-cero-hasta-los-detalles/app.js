26b3de4c458da5b455088d370a85072a7f28dc08
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2019-10-13 07:15:01 -0500

    objetos dentro de objetos
    se pueden manejar objetos dentro de objetos existentes a niveles ilimitados

---
+++ js-cero-hasta-los-detalles/app.js
@@ -0,0 +1,12 @@
+var persona = {
+    nombre: "Juana",
+    apellido: "Lopez",
+    edad: 25,
+    direccion: {
+        pais: "Costa Rica",
+        ciudad: "San Jose"
+    }
+}
+
+console.log( persona.direccion.pais );
+console.log( persona.direccion );
